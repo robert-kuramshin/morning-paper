6248fe8d154a9fa813b5434d2658070eb22e0bda
robert_kuramshin <robert.kuramshin98@gmail.com>
2018-03-30 23:45:07 -0400

    weather test basics

--- weather_test.py
+++ weather_test.py
@@ -1,23 +1,9 @@
 from weather import Weather
 weather = Weather()
+lookup = weather.lookup(12770747)
 
-# Lookup WOEID via http://weather.yahoo.com.
-
-lookup = weather.lookup(560743)
 condition = lookup.condition()
+
 print(condition.text())
+print(condition.temp())
 
-# Lookup via location name.
-
-location = weather.lookup_by_location('dublin')
-condition = location.condition()
-print(condition.text())
-
-# Get weather forecasts for the upcoming days.
-
-forecasts = location.forecast()
-for forecast in forecasts:
-    print(forecast.text())
-    print(forecast.date())
-    print(forecast.high())
-    print(forecast.low())
