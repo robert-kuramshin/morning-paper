f649a0219e4b9948d514f00ad72273d828b964ef
robert_kuramshin <robert.kuramshin98@gmail.com>
2018-04-03 01:26:07 -0400

    added news functionality

--- weather_test.py
+++ weather_test.py
@@ -1,7 +1,9 @@
 from weather import Weather
 import os
 import datetime
+from newsapi import NewsApiClient
 
+api = NewsApiClient(api_key='bbd8b254ae1a4ae1abee5c695f63fb43')
 weather = Weather()
 lookup = weather.lookup(12770747)
 condition = lookup.condition()
@@ -20,11 +22,18 @@ def image_lookup(cond_code):
         if cond_code in cond:
             return condition_files[i]
 
+articles = api.get_top_headlines(sources='bbc-news')["articles"]
+printable = []
+temp = min(len(articles),5)
+for i in range(temp):
+    printable.append(articles[i]["description"]+"\n")
+
 print_file = open("pf.txt","w")
 print_file.write(condition.text()+" "+condition.temp()+"C\n")
 print_file.write("Good Morning!\n")
 print_file.write(datetime.datetime.now().strftime("%H:%M %m/%d/%y")+"\n")
-
+for line in printable:
+    print_file.write(line)
 print_file.close()
 
 os.system('lpr -o scaling=50 '+image_lookup(condition.code()))
