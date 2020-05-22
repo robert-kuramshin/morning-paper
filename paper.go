a80c2d9f5e6ba55f06157e6861c3a7e513cb227b
robert-kuramshin <robert.kuramshin98@gmail.com>
2020-05-22 15:12:51 -0400

    automatically parsing rss feeds from url

--- paper.go
+++ paper.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"github.com/mmcdole/gofeed"
 )
 
 var line_len int = 28
+var line_brk string = "----------------------------\n"
 
 func min(a,b int) int {
 	if a < b {
@@ -14,23 +16,42 @@ func min(a,b int) int {
 	return b
 }
 
-func main() {
+func parse_feed(url string, item_limit int) string {
 	res := ""
-	fp := gofeed.NewParser()
-	feed, _ := fp.ParseURL("http://feeds.bbci.co.uk/news/world/rss.xml")//https://hackaday.com/blog/feed/"https://w1.weather.gov/xml/current_obs/KATL.rss")//
-	//fmt.Println(feed.Title)
-	for _,item := range feed.Items[:3] {
-		//fmt.Println(item.Title)
-		i := 0
-		title := item.Title
-		tlen := len(title)
-		for(i<tlen) {
-			res += title[i:min(i+line_len,tlen)]
-			res += "\n"
-			i+=line_len
-		}
-		res += "\n"
-	}
 
-	fmt.Println(res)
+	fp := gofeed.NewParser()
+	feed, _ := fp.ParseURL(url)
+
+	res += feed.Title + "\n"
+	res += line_brk
+
+	n_items := min(len(feed.Items),item_limit)
+
+	for indx,item := range feed.Items[:n_items] {
+		//fmt.Println(item.Title)
+		line := ""
+		words := strings.Fields(item.Title)
+		for _,word := range words {
+			if len(line) + len(word) + 1 > line_len {
+				res += line + "\n"
+				line = word
+			} else {
+				if(len(line) != 0) {
+					line += " "
+				}
+				line += word
+			}
+		}
+		if(indx < n_items - 1) {
+			res += "\n"
+		}
+	}
+	
+	return res
+}
+
+func main() {
+	fmt.Println(parse_feed("http://feeds.bbci.co.uk/news/world/rss.xml",3))
+	fmt.Println(parse_feed("https://hackaday.com/blog/feed",3))
+//"https://w1.weather.gov/xml/current_obs/KATL.rss")//
 }
