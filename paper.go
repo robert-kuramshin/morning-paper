f8b8004045f760edb5ee379868316aac0b0739b0
robert-kuramshin <robert.kuramshin98@gmail.com>
2020-05-22 14:26:56 -0400

    splitting lines by char count

--- paper.go
+++ paper.go
@@ -5,14 +5,32 @@ import (
 	"github.com/mmcdole/gofeed"
 )
 
-func main() {
-	fp := gofeed.NewParser()
-	feed, _ := fp.ParseURL("https://w1.weather.gov/xml/current_obs/KATL.rss")//"http://feeds.bbci.co.uk/news/world/rss.xml")//https://hackaday.com/blog/feed/
-	fmt.Println(feed.Title)
-	for _,item := range feed.Items {
-		fmt.Println(item.Title)
-		if(item.Image != nil){
-			fmt.Println(item.Image.URL)
-		}
+var line_len int = 28
+
+func min(a,b int) int {
+	if a < b {
+		return a
 	}
+	return b
+}
+
+func main() {
+	res := ""
+	fp := gofeed.NewParser()
+	feed, _ := fp.ParseURL("http://feeds.bbci.co.uk/news/world/rss.xml")//https://hackaday.com/blog/feed/"https://w1.weather.gov/xml/current_obs/KATL.rss")//
+	//fmt.Println(feed.Title)
+	for _,item := range feed.Items[:3] {
+		//fmt.Println(item.Title)
+		i := 0
+		title := item.Title
+		tlen := len(title)
+		for(i<tlen) {
+			res += title[i:min(i+line_len,tlen)]
+			res += "\n"
+			i+=line_len
+		}
+		res += "\n"
+	}
+
+	fmt.Println(res)
 }
